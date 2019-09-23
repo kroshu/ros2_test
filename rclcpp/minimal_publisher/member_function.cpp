4ac9a2cfc97ab8ed1b9aa618b1a98ba8cb64b837
ROS Developer <rosdeveloper@debian-kroshu.org>
2019-09-23 18:28:54 +0200

    CI setup

--- rclcpp/minimal_publisher/member_function.cpp
+++ rclcpp/minimal_publisher/member_function.cpp
@@ -26,12 +26,11 @@ using namespace std::chrono_literals;
 class MinimalPublisher : public rclcpp::Node
 {
 public:
-  MinimalPublisher()
-  : Node("minimal_publisher"), count_(0)
+  MinimalPublisher() :
+      Node("minimal_publisher"), count_(0)
   {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
   }
 
 private:
@@ -47,8 +46,9 @@ private:
   size_t count_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
+  setvbuf(stdout, NULL, _IONBF, 1);
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
